e4b511995c69a1766e339cb2d8cffdbc4a2ac4f9
mcalijr <martink.moc@gmail.com>
2021-10-03 18:00:07 +0200

    Created base for tomorrow
    agh'

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # etch-a-sketch
-This is Etch a Sketch project from The Odin Project. Honing DOM manipulaiton skills.
+
+This is Etch a Sketch project from The Odin Project. Honing DOM manipulaiton skills thanks to it.
+
+UPDATE AFTER FINISHING PROJECT!!!
---
+++ app.js
@@ -0,0 +1,24 @@
+const sketchArea = document.getElementById("sketchArea");
+
+function makeRows(rows, cols) {
+  sketchArea.style.setProperty("--grid-rows", rows);
+  sketchArea.style.setProperty("--grid-cols", cols);
+  for (ii = 0; ii < rows * cols; ii++) {
+    let cell = document.createElement("div");
+
+    sketchArea.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(16, 16);
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+sketchArea.addEventListener(
+  "mouseover",
+  function (event) {
+    // highlight the mouseenter target
+    event.target.style.backgroundColor = "purple";
+  },
+  false
+);
